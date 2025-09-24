525065471edf424bf1bc44a2d34b7e9bd291aec0
cidit <44065673+cidit@users.noreply.github.com>
2025-09-24 17:55:35 -0400

    lifecycle interface

---
+++ include/lifecycle.hpp
@@ -0,0 +1,17 @@
+/**
+ * this interface represents something that follows arduino's typical lifecycle such as sensors, IO pins, etc.
+ * 
+ * expected use: 
+ * - inject dependencies through the constructor. see: 
+ * - implement lifecycle methods
+ *   - begin should be called in the setup()
+ *   - update should be called in the loop()
+ */
+class ILifecycle
+{
+private:
+public:
+    virtual ~ILifecycle() {}
+    virtual void begin() = 0;
+    virtual void update() = 0;
+};
