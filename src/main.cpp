fe210202a05d292c9d8360b28e05803afc63f76e
cidit <44065673+cidit@users.noreply.github.com>
2025-09-30 21:00:45 -0400

    fixed the CrcLib not being available issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,23 @@
 #include <Arduino.h>
+#include <CrcLib.h>
 
-#include <Decodeur.h> //Pour utiliser la librairie Decodeur
+const int motor_speed_controller_pin = 6;
 
-Decodeur monDecodeur(&Serial);
-
-// #include <CrcLib>
-
-// int led_pin = 3;
-// int speed_controller = 69;
-// bool on_state = false;
-
-/*
 void setup()
 {
   CrcLib::Initialize();
-  CrcLib::SetDigitalPinMode(led_pin, OUTPUT);
-  InitializePwmOutput(speed_controller);
+  CrcLib::InitializePwmOutput(motor_speed_controller_pin);
+  Serial.begin(115200);
 }
 
 void loop()
 {
   CrcLib::Update();
-  if (millis() % 1000 == 0)
-  {
-    on_state = !on_state;
-  }
+  static unsigned char pwm_output = 0;
 
-  CrcLib::SetDigitalOutput(led_pin, on_state);
-}
-  */
+  if (millis() % 100 == 0) {
+    CrcLib::SetPwmOutput(motor_speed_controller_pin, pwm_output);
 
-/*
-
-  Mega analogWrite() test
-
-  This sketch fades LEDs up and down one at a time on digital pins 2 through 13.
-
-  This sketch was written for the Arduino Mega, and will not work on other boards.
-
-  The circuit:
-
-  - LEDs attached from pins 2 through 13 to ground.
-
-  created 8 Feb 2009
-
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/AnalogWriteMega
-
-*/
-
-// These constants won't change. They're used to give names to the pins used:
-
-const int pin = 6;
-
-void setup() {
-  pinMode(pin, OUTPUT);
-  Serial.begin(115200);
-  analogWrite(pin, 0);
-}
-
-void loop() {
-  monDecodeur.refresh();
-
-  // iterate over the pins:
-  // for (int i = 0; i <=255; i++) {
-  //   analogWrite(led_pin, i);
-    
-  //   delay(100);
-
-  //   Serial.println(i);
-  // }
-
-  
-  
-  if (monDecodeur.isAvailable()) // Si du texte a été reçu par le décodeur;
-  {
-    switch (monDecodeur.getCommand())
-    {
-    case 'S':
-      auto motor_pwm = monDecodeur.getArg(0);
-      analogWrite(pin, motor_pwm);
-      break;
-    default:
-      break;
-    }
+    pwm_output += 1; // overflows, we dont mind.
   }
 }
