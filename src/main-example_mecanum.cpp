0e59d8aaacc8b93b133e105248249769ee1c2400
VictorDVD07 <victordelsaut74@gmail.com>
2025-10-22 13:46:50 -0400

    Fix rotate mecanum

--- src/main-example_mecanum.cpp
+++ src/main-example_mecanum.cpp
@@ -35,15 +35,31 @@ void loop()
     int8_t joy_stick_state_right_X = CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK2_X);
     int8_t joy_stick_state_right_Y = CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK2_Y);
 
+    joy_stick_state_left_X = constrain(joy_stick_state_left_X, -127, 127);
+    joy_stick_state_left_Y = constrain(joy_stick_state_left_Y, -127, 127);
+    joy_stick_state_right_X = constrain(joy_stick_state_right_X, -127, 127);
+    joy_stick_state_right_Y = constrain(joy_stick_state_right_Y, -127, 127);
+
     CrcLib::SetPwmOutput(motor_speed_controller_pin_BL, joy_stick_state_left_Y);
-    CrcLib::SetPwmOutput(motor_speed_controller_pin_BR, joy_stick_state_left_Y);
+    CrcLib::SetPwmOutput(motor_speed_controller_pin_BR, -joy_stick_state_left_Y);
     CrcLib::SetPwmOutput(motor_speed_controller_pin_FL, joy_stick_state_left_Y);
-    CrcLib::SetPwmOutput(motor_speed_controller_pin_FR, joy_stick_state_left_Y);
+    CrcLib::SetPwmOutput(motor_speed_controller_pin_FR, -joy_stick_state_left_Y);
+    Serial.println("=================================");
 
     Serial.print("XG" + String(joy_stick_state_left_X)+ "\t");
     Serial.print("YG" + String(joy_stick_state_left_Y)+ "\t");
     Serial.print("XD" + String (joy_stick_state_right_X)+ "\t");
     Serial.println("YD" + String(joy_stick_state_right_Y));
+    Serial.println("================");
+    // Serial.print("BL" + String()+ "\t");
+    // Serial.print("BR" + String(-)+ "\t");
+    // Serial.print("FL" + String()+ "\t");
+    // Serial.println("FR" + String(-));
+    // Serial.print("BL" + String(motor_speed_controller_pin_BL)+ "\t");
+    // Serial.print("BR" + String(motor_speed_controller_pin_BR)+ "\t");
+    // Serial.print("FL" + String(motor_speed_controller_pin_FL)+ "\t");
+    // Serial.println("FR" + String(motor_speed_controller_pin_FR));
+
 
   }
   else {
