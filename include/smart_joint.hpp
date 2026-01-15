375e772df61214b4bd676cea4b6cb8c793721400
DohaELKHOU <dohaelk005@gmail.com>
2026-01-15 18:11:15 -0500

    PID added

--- include/smart_joint.hpp
+++ include/smart_joint.hpp
@@ -1,63 +1,66 @@
 #pragma once
 #include <Arduino.h>
+#include <PID_v1.h>
 #include "motors.hpp"
 #include "encoders.hpp"
 
-class SmartJoint {
+class SmartJoint
+{
 
 public:
     Motor &m;
     RevAbsEncoder &e;
-    
+    PID pid;
+
     float min_angle = 0;
     float max_angle = 360;
     float target_angle = 0;
     bool target_is_set = false;
 
-    float Kp = 0;
-    float Ki = 0;
-    float Kd = 0; 
+    double _input, _setpoint, _output;
 
     SmartJoint(Motor &motor, RevAbsEncoder &encoder)
-        : m(motor), e(encoder) {
+        : m(motor),
+          e(encoder),
+          pid(&this->_input, &this->_output, &this->_setpoint, 0.0, 0.0, 0.0, DIRECT)
+    {
+        this->_setpoint = 0;
+    }
+
+    void begin(float kp, float ki, float kd)
+    {
+        // initialize PID
+        pid.SetTunings(kp, ki, kd);
+        pid.SetMode(AUTOMATIC);
+        pid.SetOutputLimits(-1, 1); // motor power limits
     }
 
     // called once per loop, if needed
-    void update() {
-        if (target_is_set && e.is_angle_initialized()) {
+    void update()
+    {
+        if (target_is_set && e.is_angle_initialized())
+        {
             float current_angle = e.read_angle();
-            float error = target_angle - current_angle;
+            float error = target_angle - current_angle; // not really used
 
-            float kP = 0.01;
-            float kI = 0.001;
-            float kD = 0.001;
+            this->_input = current_angle;
+            this->_setpoint = target_angle;
 
-            static float integral = 0;
-            static float last_error = 0;
-
-            integral += error;
-            float derivative = error - last_error;
-            last_error = error;
-
-            float power = kP * error + kI * integral + kD * derivative;
-            m.set_power(power);
+            this->pid.Compute();
+            m.set_power(this->_output);
         }
     }
 
-    void set_angular_limits(float limMin, float limMax) {
-        min_angle = limMin;
-        max_angle = limMax;
-    }
+    // void set_angular_limits(float limMin, float limMax)
+    // {
+    //     min_angle = limMin;
+    //     max_angle = limMax;
+    // }
 
-    void set_target_angle(float target) {
+    void set_target_angle(float target)
+    {
         // set the angle at which the joint must move to
         target_angle = constrain(target, min_angle, max_angle);
         target_is_set = true;
-    }
-
-    void set_pid(float kp, float ki, float kd) {
-        Kp = kp;
-        Ki = ki;
-        Kd = kd;
     }
 };
--- src/main-example_smart_motors.cpp
+++ src/main-example_smart_motors.cpp
@@ -2,10 +2,14 @@
 #include <CrcLib.h>
 #include "motors.hpp"
 #include "encoders.hpp"
+#include "smart_joint.hpp"
 
-Encoder encodeur(CRC_DIG_1, 2000);
+RevAbsEncoder encodeur(CRC_DIG_1, 2000);
 Motor moteur(CRC_PWM_1);
 // unsigned long pulseWidth = 0;
+
+SmartJoint joint(moteur, encodeur);
+
 
 void setup()
 {
@@ -16,13 +20,14 @@ void setup()
     //                         // Ou utiliser une librairie comme "PulsePosition" ou "InputCapture"
 
     moteur.setup();
+    joint.begin(0.0001, 0.0, 0.0005);
 }
 
 void loop()
 {
     CrcLib::Update();
 
-    Serial.println("current: " + encodeur.getCurrentValue());
+    Serial.println("current: " + String(encodeur._read_raw()) + " // angle: " + String(encodeur.read_angle()));
     
     // Mesure approximative (non fiable pour 975 Hz)
     // auto pulseWidth = pulseIn(encoder, HIGH, 2000); // Timeout 2ms
@@ -38,8 +43,10 @@ void loop()
     //     Serial.println(angle);
     // }
 
+    joint.set_target_angle(180);
+    joint.update();
 
-    moteur.set_power(0.5);
+    // moteur.set_power(0.5);
 }
 
 
